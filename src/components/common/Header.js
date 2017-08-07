830fc781403d88b62f13d70ea6b6087554af4419
Your Name <you@example.com>
2017-08-07 16:12:58 +0900

    CSS グリットでヘッダーを作成した。形のみでイベントロジック、リンク先はまだ無い。

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,15 +1,61 @@
 import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
+// import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
-const Header = () => {
-  return (
-    <nav className="nav nav--center">
-      <IndexLink to="/" className="" activeClassName="active">
-        <span className="glyphicon glyphicon-menu-hamburger"></span>
-      </IndexLink>
-      {" | "}
-      <Link to="/about" className="" activeClassName="active">About</Link>
-    </nav>
-  );
-};
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div id="container" className="link_box">
+        <div id="itemA"></div>
+        <div id="itemB">
+          <a href="#">
+            <img src=" http://cdn.dscount.com/images_2016/top/ntop_all02.jpg" alt="dscount"/>
+          </a>
+        </div>
+        <div id="itemC">
+          <a href="#">
+            <img src=" http://cdn.dscount.com/images_2016/top/btn_dscoupon02.jpg" alt="dscount"/>
+          </a>
+        </div>
+        <div id="itemD"></div>
+        <div id="itemE">
+          <a href="#" >ログイン</a>
+        </div>
+        <div id="itemF">
+          <a href="#" >会員登録</a>
+        </div>
+        <div id="itemG">
+          <a href="#" >マイページ{" "}▼</a>
+        </div>
+        <div id="itemH">
+          <a href="#" >お客様センター{" "}▼</a>
+        </div>
+        <div id="itemI">
+          <a href="#" >
+            <span className="glyphicon glyphicon-shopping-cart">(0)</span>
+          </a>
+        </div>
+        <div id="itemJ">
+          <a href="#" >
+            <span className="glyphicon glyphicon-heart">(1)</span>
+          </a>
+        </div>
+        <form id="itemK">
+          <div className="field">
+            <button className="drop-down-btn" type="button" id="search">総合検索{" "}▼</button>
+            <input className="header-search-input" type="text" id="searchterm" placeholder=" what do you want ?" />
+            <button className="search-submit" type="button" id="search">
+              <span className="glyphicon glyphicon-search"></span>
+            </button>
+          </div>
+        </form>
+        <div id="itemL"></div>
+      </div>
+    );
+  }
+}
+
 export default Header;
