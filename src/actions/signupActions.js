1b51304c4460ba49a133f56602846d4449f3d59d
Your Name <you@example.com>
2017-08-28 18:28:59 +0800

    Redux Flow : Action Type Constants

--- src/actions/signupActions.js
+++ src/actions/signupActions.js
@@ -1,3 +1,5 @@
+import * as types from './actionTypes';
+
 export function userSignupRequest(userData) {
-  return { type: 'CREATE_SIGNUP', userData}
+  return { type: types.CREATE_SIGNUP, userData };
 }
