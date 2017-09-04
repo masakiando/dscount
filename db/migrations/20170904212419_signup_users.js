7bab11fe3bb5ad41fe62b46a746b3d1d7b722d59
Your Name <you@example.com>
2017-09-05 04:37:42 +0800

    Edit knex.schema.createTable, signup users

--- db/migrations/20170904212419_signup_users.js
+++ db/migrations/20170904212419_signup_users.js
@@ -1,8 +1,27 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', function(table){
+    table.increments();
+
+    table.string('name').notNullable().unique();
+    table.string('username').notNullable().unique();
+    table.string('password_digest').notNullable();
+
+    table.integer('birthdayY').notNullable();
+    table.integer('birthdayM').notNullable();
+    table.integer('birthdayD').notNullable();
+    table.string('gender').notNullable();
+    table.integer('postalCode').notNullable();
+    table.string('address').notNullable();
+    table.string('email').notNullable().unique();
+    table.integer('mobilePhoneNumber').notNullable().unique();
+    table.boolean('receiveEmail').notNullable();
+    table.boolean('receiveSNS').notNullable();
+    table.boolean('suspended').notNullable().defaultTo(false);;
+
+    table.timestamps();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users');
 };
