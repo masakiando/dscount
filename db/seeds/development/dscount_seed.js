d54429ecc485ab6ada2cf3dabedc16d7c034dfe3
Your Name <you@example.com>
2017-09-05 23:29:22 +0800

    add ,

--- db/seeds/development/dscount_seed.js
+++ db/seeds/development/dscount_seed.js
@@ -6,14 +6,14 @@ exports.seed = function(knex, Promise) {
         username: 'YamadaTaro',
         password_digest: 'password1',
         birthdayY: 1980,
-        birthdayM: 1
-        birthdayD: 1
-        gender: 'male'
-        postalCode: 1111111
-        address: '台南安平区1-1-1'
-        email: 'anpin@gmail.com'
-        mobilePhoneNumber: 09012345678
-        receiveEmail: true
+        birthdayM: 1,
+        birthdayD: 1,
+        gender: 'male',
+        postalCode: 1111111,
+        address: '台南安平区1-1-1',
+        email: 'anpin@gmail.com',
+        mobilePhoneNumber: 09012345678,
+        receiveEmail: true,
         receiveSNS: true
       });
     }).then(function () {
@@ -22,14 +22,14 @@ exports.seed = function(knex, Promise) {
         username: 'YamadaHanako',
         password_digest: 'password2',
         birthdayY: 1980,
-        birthdayM: 2
-        birthdayD: 2
-        gender: 'female'
-        postalCode: 2222222
-        address: '台南安平区2-2-2'
-        email: 'anpin2@gmail.com'
-        mobilePhoneNumber: 08012345678
-        receiveEmail: true
+        birthdayM: 2,
+        birthdayD: 2,
+        gender: 'female',
+        postalCode: 2222222,
+        address: '台南安平区2-2-2',
+        email: 'anpin2@gmail.com',
+        mobilePhoneNumber: 08012345678,
+        receiveEmail: true,
         receiveSNS: true
       });
     })
